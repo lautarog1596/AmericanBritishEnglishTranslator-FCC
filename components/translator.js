2e174f27dcb6b96dfb5d7c0ee0cee7c0ba93bcaa
lautarog1596 <lautarog1596@gmail.com>
2022-05-06 12:24:53 -0300

    Add api.js and translator.js

--- components/translator.js
+++ components/translator.js
@@ -5,6 +5,105 @@ const britishOnly = require('./british-only.js')
 
 class Translator {
 
+    reverseDict(dict) {
+        return Object.assign({}, // target 
+            ...Object.entries(dict).map(([key, value]) => ({
+                [value]: key
+            }))); // source
+    }
+
+    toBritishEnglish(text) {
+        const dict = {...americanOnly, ...americanToBritishSpelling };
+        const titles = americanToBritishTitles
+        const timeRegex = /([1-9]|1[012]):[0-5][0-9]/g
+        const translated = this.translate(
+            text,
+            dict,
+            titles,
+            timeRegex,
+            'toBritish'
+        )
+        if (!translated) return text
+        return translated
+    }
+
+    toAmericanEnglish(text) {
+        const dict = {...britishOnly, ...reverseDict(americanToBritishSpelling) };
+        const titles = reverseDict(americanToBritishTitles)
+        const timeRegex = /([1-9]|1[012]).[0-5][0-9]/g
+        const translated = this.translate(
+            text,
+            dict,
+            titles,
+            timeRegex,
+            'toAmerican'
+        )
+        if (!translated) return text
+        return translated
+    }
+
+    translate(text, dict, titles, timeRegex, locale) {
+        const lowerText = text.toLowerCase()
+        const matchesMap = {}
+
+        // Search for titles/honorifics and add`em to the matchesMap object
+        Object.entries(titles).map(([key, value]) => {
+            if (lowerText.includes(key)) {
+                matchesMap[key] = value.charAt(0).toUpperCase() + value.slice(1)
+            }
+        })
+
+        // Filter words with spaces from current dictionary
+        const wordsWithSpace = Object.fromEntries(
+            Object.entries(dict).filter(([key, value]) => key.includes(' '))
+        )
+
+        // Search for spaced word matches and add`em to the matchesMap object
+        Object.entries(wordsWithSpace).map(([key, value]) => {
+            if (lowerText.includes(key)) {
+                matchesMap[key] = value
+            }
+        })
+
+        // Search for individual word matches and add`em to the matchesMap object
+        lowerText.match(/(\w+([-'])(\w+)?['-]?(\w+))|\w+/g).forEach(word => {
+            if (dict[word]) matchesMap[word] = dict[word]
+        })
+
+        // Search for time matches and add`em to the matchesMap object
+        const matchedTimes = lowerText.match(timeRegex)
+
+        if (matchedTimes) {
+            matchedTimes.map(time => {
+                if (locale === 'toBritish')
+                    return matchesMap[time] = time.replace(':', '.')
+                return matchesMap[time] = time.replace('.', ':')
+            })
+        }
+
+        // No matches
+        if (Object.keys(matchesMap).length === 0) return null
+
+        console.log('matchesMap', matchesMap)
+
+        const translation = this.replaceAll(text, matchesMap)
+        const translationWithHighlights = this.replaceAllWithHighlight(text, matchesMap)
+
+        return [translation, translationWithHighlights]
+    }
+
+    replaceAll(text, matchesMap) {
+        const re = new RegExp(Object.keys(matchesMap).join('|'), 'gi') // Regular expression for all keys in the matchesMap object g: global, i: case-insensitive
+        return text.replace(re, matched => matchesMap[matched.toLowerCase()])
+    }
+
+    replaceAllWithHighlight(text, matchesMap) {
+        const re = new RegExp(Object.keys(matchesMap).join('|'), 'gi')
+        return text.replace(re, matched => {
+            return `<span class="highlight">${matched.toLowerCase()}</span>`
+        })
+    }
+
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -2,12 +2,36 @@
 
 const Translator = require('../components/translator.js');
 
-module.exports = function (app) {
-  
-  const translator = new Translator();
+module.exports = function(app) {
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
-    });
+    const translator = new Translator();
+
+    app.route('/api/translate')
+        .post((req, res) => {
+            const { text, locale } = req.body;
+            if (!locale || text === undefined) {
+                res.json({ error: 'Required field(s) missing' });
+                return;
+            }
+            if (text == '') {
+                res.json({ error: 'No text to translate' });
+                return
+            }
+
+            let translation = '';
+            if (locale == 'american-to-british') {
+                translation = translator.toBritishEnglish(text);
+            } else if (locale == 'british-to-american') {
+                translation = translator.toAmericanEnglish(text);
+            } else {
+                res.json({ error: 'Invalid value for locale field' });
+                return;
+            }
+            if (translation === text || !translation) {
+                res.json({ text, translation: 'Everything looks good to me!' });
+                return;
+            } else {
+                res.json({ text, translation: translation[1] }); // second translation is going to be with highlights
+            }
+        });
 };
